150069058d1126f80c50b487542ae4ce4dcb977d
Katherine Gardner <katiegardner@Katherines-MacBook-Air.local>
2021-07-29 14:09:20 -0700

    adding endpoint

--- server/index.js
+++ server/index.js
@@ -6,8 +6,14 @@ const app = express ();
 
 const port = process.env.PORT || 5000
 
+app.use(express.static(path.join(__dirname, '/public/images')) )
+
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+
+
+
+
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)})
